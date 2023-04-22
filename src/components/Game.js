78be9aa8c20286085d271cf60afd7eda33f4a3dc
jaypante <jaypante12@gmail.com>
2023-04-22 16:43:53 +0800

    fetching game details from api

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
-
+import  {useDispatch} from 'react-redux'
+import { loadDetail } from '../actions/detailAction'
 
 const Game = ({name, released, image, id,}) => {
+
+  const dispatch = useDispatch()
+  const loadDetailHandler = ()=>{
+    dispatch(loadDetail(id))
+  }
   return (
-    <StyledGame>
+    <StyledGame onClick={loadDetailHandler}>
         <h3>{name}</h3>
         <p>{released} </p>
         <img src={image} alt={name} />
@@ -18,6 +24,7 @@ const StyledGame = styled(motion.div)`
     box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.3);
     text-align: center;
     border-radius: 1rem;
+    cursor: pointer;
     img{
         width: 100%;
         height: 40vh;
