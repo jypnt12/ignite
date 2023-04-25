f380820f4f5819583ad7a3d08391b1a5e37acb2b
jaypante <jaypante12@gmail.com>
2023-04-25 22:28:39 +0800

    adding rating stars

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -11,6 +11,8 @@ import xbox from '../img/xbox.svg'
 import nintendo from '../img/nintendo.svg'
 import apple from '../img/apple.svg'
 import gamepad from '../img/gamepad.svg'
+import starEmpty from '../img/star-empty.png'
+import starFull from '../img/star-full.png'
 
 const GameDetails = ({pathId}) => {
     const navigate = useNavigate()
@@ -22,6 +24,20 @@ const GameDetails = ({pathId}) => {
             navigate('/')
         }
     }
+
+    const getStars =()=>{
+        const stars = [];
+        const rating = Math.floor(game.rating);
+        for(let i = 1; i<=5; i++){
+            if (i<+rating){
+                stars.push(<img alt="star" key={i} src={starFull}></img>)
+            }else{
+                stars.push(<img alt="star" key={i} src={starFull}></img>)
+            }
+        }
+        return stars
+    }
+
     //get platfoprm images
     const getPlatform = (platform) => {
         switch(platform){
@@ -48,6 +64,7 @@ const GameDetails = ({pathId}) => {
                         <div className="rating">
                             <h3>{game.name}</h3>
                             <p>Rating: {game.rating}</p>
+                            {getStars()}
                         </div>
                         <Info>
                             <h3>Platforms</h3>
@@ -112,6 +129,11 @@ const Stats= styled(motion.div)`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    img{
+        width: 2rem;
+        height: 2rem;
+        display: inline;
+    }
 `
 const Info = styled(motion.div)`
     text-align: center;
